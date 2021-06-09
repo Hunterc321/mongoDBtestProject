f9fabea3b5c55cf14f6dcea69c2d486e086d7051
popacosmin <popa.cosmin@ac.tuiasi.ro>
2021-06-09 16:46:18 +0300

    Initial commit

---
+++ src/main/java/com/example/mongodbtestproject/MongoDBtestProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.mongodbtestproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoDBtestProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MongoDBtestProjectApplication.class, args);
+    }
+
+}
